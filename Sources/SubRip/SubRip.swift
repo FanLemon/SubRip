4d98fbef78507009ea4ece6bad6f560a6c7ad456
FanLemon <iyangy@gmail.com>
2021-12-14 11:38:23 +0900

    Init Version

--- Sources/SubRip/SubRip.swift
+++ Sources/SubRip/SubRip.swift
@@ -1,6 +1,476 @@
-public struct SubRip {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+
+
+///
+/// `SubRip` (SubRip Text) files are named with the extension .srt,
+/// and contain formatted lines of plain text in groups separated by a blank line.
+/// Subtitles are numbered sequentially, starting at 1.
+/// The timecode format used is hours:minutes:seconds,milliseconds with time units fixed to two zero-padded digits
+/// and fractions fixed to three zero-padded digits (00:00:00,000).
+/// The fractional separator used is the comma, since the program was written in France.
+///
+/// A numeric counter identifying each sequential subtitle
+/// The time that the subtitle should appear on the screen, followed by --> and the time it should disappear
+/// Subtitle text itself on one or more lines
+/// A blank line containing no text, indicating the end of this subtitle
+/// `Example`:
+///
+/// 1
+/// 00:02:16,612 --> 00:02:19,376
+/// Senator, we're making
+/// our final approach into Coruscant.
+///
+/// 2
+/// 00:02:19,482 --> 00:02:21,609
+/// Very good, Lieutenant.
+///
+/// 3
+/// 00:03:13,336 --> 00:03:15,167
+/// We made it.
+///
+/// 4
+/// 00:03:18,608 --> 00:03:20,371
+/// I guess I was wrong.
+///
+/// 5
+/// 00:03:20,476 --> 00:03:22,671
+/// There was no danger at all.
+///
+
+protocol TimeIntegerLiteral {
+    
+    static var numeralBase: Int { get }
+    
+    static var literalWidth: Int { get }
+    
+    var integerValue: Int { get set }
+    
+    init(intValue: Int)
+    
+    var description: String { get }
+    
+    static func time(value: Int) -> Self
+    
+    static func time(str: String) -> Self
+}
+
+
+extension TimeIntegerLiteral {
+    
+    static func time(value: Int) -> Self {
+        return Self.init(intValue: min(max(value, 0), Self.numeralBase-1))
+    }
+    
+    static func time(str: String) -> Self {
+        guard let value = Int(str) else {
+            return Self.init(intValue: 0)
+        }
+        
+        return Self.time(value: value)
+    }
+    
+    var description: String {
+        return String(format: "%0\(Self.literalWidth)d", integerValue)
+    }
+    
+    static func +(lhs: Self, rhs: Int) -> Int {
+        return lhs.integerValue + rhs
+    }
+    
+    static func -(lhs: Self, rhs: Int) -> Int {
+        return lhs.integerValue - rhs
+    }
+    
+    static func *(lhs: Self, rhs: Int) -> Int {
+        return lhs.integerValue * rhs
+    }
+    
+    static func /(lhs: Self, rhs: Int) -> Int {
+        guard rhs != 0 else {
+            return 0
+        }
+        return lhs.integerValue / rhs
+    }
+}
+
+
+struct TimeNumeraBase24: TimeIntegerLiteral {
+    
+    static var numeralBase: Int = 24
+    
+    static var literalWidth: Int = 2
+    
+    var integerValue: Int = 0
+    
+    init(intValue: Int) {
+        integerValue = intValue
+    }
+}
+
+
+struct TimeNumeraBase60: TimeIntegerLiteral {
+    
+    static var numeralBase: Int = 60
+    
+    static var literalWidth: Int = 2
+    
+    var integerValue: Int = 0
+    
+    init(intValue: Int) {
+        integerValue = intValue
+    }
+}
+
+
+struct TimeNumeraBase1000: TimeIntegerLiteral {
+    
+    static var numeralBase: Int = 1000
+    
+    static var literalWidth: Int = 3
+    
+    var integerValue: Int = 0
+    
+    init(intValue: Int) {
+        integerValue = intValue
+    }
+}
+
+
+///
+/// `Zero-padded Timecode`:
+///
+/// hours:minutes:seconds,milliseconds
+/// 00:03:18,608
+///
+struct Timecode: Equatable {
+    
+    var hours: TimeNumeraBase24
+    var minutes: TimeNumeraBase60
+    var seconds: TimeNumeraBase60
+    var milliseconds: TimeNumeraBase1000
+    
+    init() {
+        hours = TimeNumeraBase24(intValue: 0)
+        minutes = TimeNumeraBase60(intValue: 0)
+        seconds = TimeNumeraBase60(intValue: 0)
+        milliseconds = TimeNumeraBase1000(intValue: 0)
+    }
+    
+    static func split(_ description: String) -> [String]? {
+        /// 0  1  2
+        /// -- -- ------
+        /// 00:03:18,608
+        let hmsComponents = description.components(separatedBy: ":")
+        guard hmsComponents.count == 3 else {
+            return nil
+        }
+        
+        guard hmsComponents[0].count == 2 else {
+            return nil
+        }
+        
+        guard hmsComponents[1].count == 2 else {
+            return nil
+        }
+        
+        guard hmsComponents[2].count == 6 else {
+            return nil
+        }
+        
+        let sm = hmsComponents[2].components(separatedBy: ",")
+        guard sm.count == 2 else {
+            return nil
+        }
+        
+        guard sm[0].count == 2 else {
+            return nil
+        }
+        
+        guard sm[1].count == 3 else {
+            return nil
+        }
+        
+        return [hmsComponents[0], hmsComponents[1], sm[0], sm[1]]
+    }
+    
+    init?(_ description: String) {
+        guard let hmsm = Timecode.split(description) else {
+            return nil
+        }
+        
+        hours = TimeNumeraBase24.time(str: hmsm[0])
+        minutes = TimeNumeraBase60.time(str: hmsm[1])
+        seconds = TimeNumeraBase60.time(str: hmsm[2])
+        milliseconds = TimeNumeraBase1000.time(str: hmsm[3])
+    }
+    
+    var description: String {
+        return hours.description + ":" + minutes.description + ":" + seconds.description + "," + milliseconds.description
+    }
+
+    static let MILLISECONDS_PER_SECOND = 1000
+    static let MILLISECONDS_PER_MINUTE = 60 * Timecode.MILLISECONDS_PER_SECOND
+    static let MILLISECONDS_PER_HOUR = 60 * Timecode.MILLISECONDS_PER_MINUTE
+    
+    init(milliseconds ms: Int) {
+        var millis = max(ms, 0)
+        
+        let hour = millis / Timecode.MILLISECONDS_PER_HOUR
+        millis -= (hour * Timecode.MILLISECONDS_PER_HOUR)
+        
+        let minute = millis / Timecode.MILLISECONDS_PER_MINUTE
+        millis -= minute * Timecode.MILLISECONDS_PER_MINUTE
+        
+        let second = millis / Timecode.MILLISECONDS_PER_SECOND
+        millis -=  second * Timecode.MILLISECONDS_PER_SECOND
+        
+        let millisecond = millis
+        
+        hours = TimeNumeraBase24(intValue: hour)
+        minutes = TimeNumeraBase60(intValue: minute)
+        seconds = TimeNumeraBase60(intValue: second)
+        milliseconds = TimeNumeraBase1000(intValue: millisecond)
+    }
+    
+    var millisecondsValue: Int {
+        let mmm = milliseconds.integerValue
+        let sss = seconds * Timecode.MILLISECONDS_PER_SECOND
+        let min = minutes * Timecode.MILLISECONDS_PER_MINUTE
+        let hhh = hours * Timecode.MILLISECONDS_PER_HOUR
+        
+        return hhh + min + sss + mmm
+    }
+    
+    static func +(lhs: Timecode, rhs: Int) -> Timecode {
+        return Timecode(milliseconds: lhs.millisecondsValue + rhs)
+    }
+    
+    static func -(lhs: Timecode, rhs: Int) -> Timecode {
+        return Timecode(milliseconds: lhs.millisecondsValue - rhs)
+    }
+    
+    static func +(lhs: Timecode, rhs: Timecode) -> Timecode {
+        return Timecode(milliseconds: lhs.millisecondsValue + rhs.millisecondsValue)
+    }
+    
+    static func -(lhs: Timecode, rhs: Timecode) -> Timecode {
+        return Timecode(milliseconds: lhs.millisecondsValue - rhs.millisecondsValue)
+    }
+    
+    static func == (lhs: Timecode, rhs: Timecode) -> Bool {
+        return lhs.millisecondsValue == rhs.millisecondsValue
+    }
+    
+}
+
+
+///
+/// 00:02:19,482 --> 00:02:21,609
+///
+struct TimeDuration: Equatable {
+    
+    static let delimiterBlock = " --> "
+    
+    var start: Timecode
+    var end: Timecode
+    
+    init(startTime: Timecode, endTime: Timecode) {
+        start = startTime
+        end = endTime
+    }
+    
+    init?(_ description: String) {
+        let sample = "00:02:19,482 --> 00:02:21,609"
+        
+        guard sample.count == description.count else {
+            return nil
+        }
+        
+        let parts = description.components(separatedBy: TimeDuration.delimiterBlock)
+        guard parts.count == 2, parts[0].count == parts[1].count else {
+            return nil
+        }
+        
+        guard let t1 = Timecode(parts[0]), let t2 = Timecode(parts[1]) else {
+            return nil
+        }
+        
+        start = t1
+        end = t2
+    }
+    
+    var description: String {
+        return start.description + TimeDuration.delimiterBlock + end.description
+    }
+    
+    var milliseconds: Int {
+        return (end - start).millisecondsValue
+    }
+    
+    static func +(lhs: TimeDuration, rhs: Int) -> TimeDuration {
+        return TimeDuration(startTime: lhs.start + rhs, endTime: lhs.end + rhs)
+    }
+    
+    static func -(lhs: TimeDuration, rhs: Int) -> TimeDuration {
+        return TimeDuration(startTime: lhs.start - rhs, endTime: lhs.end - rhs)
+    }
+    
+    static func == (lhs: TimeDuration, rhs: TimeDuration) -> Bool {
+        return lhs.start == rhs.start && lhs.end == rhs.end
+    }
+    
+}
+
+
+struct Subtitle: Equatable {
+    
+    static let newline = "\n"
+    
+    var sequentialNumber: Int
+    var time: TimeDuration
+    var text: String
+    let blank: String = Subtitle.newline
+    
+    var description: String {
+        return String(sequentialNumber) + Subtitle.newline
+        + time.description + Subtitle.newline
+        + text + blank
+    }
+    
+    static func +(lhs: Subtitle, rhs: Int) -> Subtitle {
+        var result = lhs
+        result.time = lhs.time + rhs
+        return result
+    }
+    
+    static func == (lhs: Subtitle, rhs: Subtitle) -> Bool {
+        return lhs.sequentialNumber == rhs.sequentialNumber &&
+                lhs.time == rhs.time &&
+                lhs.text == rhs.text
+    }
+    
+    func split() -> [Subtitle] {
+        let (left, right) = Subtitle.divideText(text: text)
+        
+        return [Subtitle(sequentialNumber: sequentialNumber, time: time, text: left),
+                Subtitle(sequentialNumber: sequentialNumber, time: time, text: right)]
+    }
+    
+    static func divideText(text: String) -> (String, String) {
+        var left = ""
+        var right = ""
+        
+        let lines = text.components(separatedBy: CharacterSet.newlines)
+        
+        switch lines.count {
+        case 2:
+            left = lines[0] + "\n"
+            right = lines[0] + "\n"
+        case 3:
+            left = lines[0] + "\n"
+            right = lines[1] + "\n"
+        case 4:
+            left = lines[0] + "\n"
+            right = lines[1] + lines[2] + "\n"
+        case 5:
+            left = lines[0] + lines[1] + "\n"
+            right = lines[2] + lines[3] + "\n"
+        case 6:
+            left = lines[0] + lines[1] + lines[2] + "\n"
+            right = lines[3] + lines[4] + lines[5] + "\n"
+            
+        default:
+            left = lines[0] + "\n"
+            for i in 1 ..< lines.count {
+                right += lines[i] + "\n"
+            }
+        }
+        
+        return (left, right)
+    }
+}
+
+
+struct SubRip: Equatable {
+    
+    var subtitles: [Subtitle]
+    
+    init(subs: [Subtitle]) {
+        subtitles = subs
+    }
+    
+    init(srtFileContent: String) {
+        let sourceLines = srtFileContent.components(separatedBy: CharacterSet.newlines)
+        
+        // Append 2 fake lines for reducing check last time
+        let lines = sourceLines + ["99999", "00:02:19,482 --> 00:02:21,609"]
+        
+        let (subs, _) = lines.reduce(into: ([Subtitle](), [String]())) { partialResult, line in
+            // 0. Caches Lines
+            partialResult.1.append(line)
+            
+            // 1. Detect Timecode
+            if nil != TimeDuration(line) {
+                // number
+                // time
+                // xxx
+                // number
+                // time
+                let stripedLines = partialResult.1.filter { s in
+                    return s.count > 0
+                }
+                
+                let timeLines = stripedLines.compactMap { s in
+                    return TimeDuration(s)
+                }
+                
+                if stripedLines.count >= 5, timeLines.count >= 2 {
+                    // 2. Build Subtitle
+                    let number = partialResult.0.count + 1
+                    let timeDesc = timeLines[0]
+                    let text = stripedLines[2..<stripedLines.count-2].reduce(into: "") { p, t in
+                        p += t
+                        p += "\n"
+                    }
+                    
+                    partialResult.0.append(Subtitle(sequentialNumber: number, time: timeDesc, text: text))
+                    
+                    // 3. Clear cachedLines
+                    partialResult.1.removeAll()
+                    partialResult.1.append(contentsOf: stripedLines[(stripedLines.count-2)...])
+                }
+            }
+        }
+        
+        subtitles = subs
+    }
+    
+    var srtFileContent: String {
+        return subtitles.reduce("") { partialResult, s in
+            return partialResult + s.description
+        }
+    }
+    
+    static func +(lhs: SubRip, rhs: Int) -> SubRip {
+        let subs = lhs.subtitles.map { sub in
+            return sub + rhs
+        }
+        
+        return SubRip(subs: subs)
+    }
+    
+    func split() -> [SubRip] {
+        var left = [Subtitle]()
+        var right = [Subtitle]()
+        
+        subtitles.forEach { sub in
+            let splited = sub.split()
+            left.append(splited[0])
+            right.append(splited[1])
+        }
+        
+        return [SubRip(subs: left),
+                SubRip(subs: right)]
     }
 }
--- Tests/SubRipTests/SubRipTests.swift
+++ Tests/SubRipTests/SubRipTests.swift
@@ -1,11 +1,270 @@
 import XCTest
 @testable import SubRip
 
+
+extension TimeIntegerLiteral {
+    
+    static var maxIntegerValue: Int {
+        return Self.numeralBase - 1
+    }
+    
+    static var minIntegerValue: Int {
+        return 0
+    }
+    
+    func validateTimeIntegerLiteralEqual(input: Int) throws {
+        XCTAssertEqual(integerValue, input)
+        
+        var label = String(input)
+        for _ in 0 ..< Self.literalWidth-label.count {
+            label = "0" + label
+        }
+        XCTAssertEqual(description, label)
+    }
+    
+    static func validateTimeIntegerLiteralMax(input: Int) throws {
+        let result = Self.time(value: input)
+        
+        try result.validateTimeIntegerLiteralEqual(input: Self.maxIntegerValue)
+    }
+    
+    static func validateTimeIntegerLiteralMax(input: String) throws {
+        let result = Self.time(str: input)
+        
+        try result.validateTimeIntegerLiteralEqual(input: Self.maxIntegerValue)
+    }
+    
+    static func validateTimeIntegerLiteralMin(input: Int) throws {
+        let result = Self.time(value: input)
+        
+        try result.validateTimeIntegerLiteralEqual(input: Self.minIntegerValue)
+    }
+    
+    static func validateTimeIntegerLiteralMin(input: String) throws {
+        let result = Self.time(str: input)
+        
+        try result.validateTimeIntegerLiteralEqual(input: Self.minIntegerValue)
+    }
+    
+    static func validateTimeIntegerLiteralNormal(input: Int) throws {
+        let result = Self.time(value: input)
+        
+        try result.validateTimeIntegerLiteralEqual(input: input)
+    }
+    
+    static func validateTimeIntegerLiteralNormal(input: String) throws {
+        let result = Self.time(str: input)
+        
+        try result.validateTimeIntegerLiteralEqual(input: Int(input) ?? 0)
+    }
+    
+    static func testTimeIntegerLiteral() throws {
+        try Self.validateTimeIntegerLiteralMin(input: -3)
+        try Self.validateTimeIntegerLiteralMin(input: 0)
+        try Self.validateTimeIntegerLiteralNormal(input: Int.random(in: 1 ..< Self.numeralBase))
+        try Self.validateTimeIntegerLiteralNormal(input: Int.random(in: 1 ..< Self.numeralBase))
+        try Self.validateTimeIntegerLiteralNormal(input: Int.random(in: 1 ..< Self.numeralBase))
+        try Self.validateTimeIntegerLiteralMax(input: Self.numeralBase)
+        try Self.validateTimeIntegerLiteralMax(input: Self.numeralBase + 13)
+        
+        try Self.validateTimeIntegerLiteralMin(input: "")
+        try Self.validateTimeIntegerLiteralMin(input: "a")
+        try Self.validateTimeIntegerLiteralMin(input: "fdjsaklf")
+        try Self.validateTimeIntegerLiteralMin(input: "-4")
+        try Self.validateTimeIntegerLiteralMin(input: "0")
+        try Self.validateTimeIntegerLiteralNormal(input: String(Int.random(in: 1 ..< Self.numeralBase)))
+        try Self.validateTimeIntegerLiteralNormal(input: String(Int.random(in: 1 ..< Self.numeralBase)))
+        try Self.validateTimeIntegerLiteralNormal(input: String(Int.random(in: 1 ..< Self.numeralBase)))
+        try Self.validateTimeIntegerLiteralMax(input: String(Self.numeralBase))
+        try Self.validateTimeIntegerLiteralMax(input: String(Self.numeralBase + 13))
+    }
+}
+
+
+
+enum SubRipTestsError: Error {
+    
+    case encounteredNil
+}
+
+
 final class SubRipTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SubRip().text, "Hello, World!")
+
+    func testTimeIntegerLiteral() throws {
+        try TimeNumeraBase24.testTimeIntegerLiteral()
+        try TimeNumeraBase60.testTimeIntegerLiteral()
+        try TimeNumeraBase1000.testTimeIntegerLiteral()
+    }
+    
+    func testTimecode() throws {
+        var sample = "00:03:18,608"
+
+        guard let t1 = Timecode(sample) else {
+            throw SubRipTestsError.encounteredNil
+        }
+        
+        let t2 = Timecode(milliseconds: t1.millisecondsValue)
+        XCTAssertEqual(t1, t2)
+        
+        
+        sample = "00:03:18608"
+        XCTAssertNil(Timecode(sample))
+        
+        
+        sample = "00:03:18:608"
+        XCTAssertNil(Timecode(sample))
+        
+        
+        sample = "00,03,18:608"
+        XCTAssertNil(Timecode(sample))
+        
+        
+        let t3 = t1 + 392
+        guard let t4 = Timecode(t3.description) else {
+            throw SubRipTestsError.encounteredNil
+        }
+        XCTAssertEqual(t3, t4)
+        let t5 = t4 - 392
+        XCTAssertEqual(t1, t5)
+    }
+    
+    func testTimeduration() throws {
+        var sample = "00:02:19,482 --> 00:02:21,609"
+        
+        guard let td1 = TimeDuration(sample) else {
+            throw SubRipTestsError.encounteredNil
+        }
+        
+        let td2 = TimeDuration(startTime: td1.start + 3845923 - 3845923, endTime: td1.end + 3845923 - 3845923)
+        XCTAssertEqual(td1, td2)
+        
+        sample = "00:02:19,482 -->00:02:21,609"
+        XCTAssertNil(TimeDuration(sample))
+        
+        sample = "00:02:19,482 --- 00:02:21,609"
+        XCTAssertNil(TimeDuration(sample))
+        
+        sample = "00:02:19,482 <-- 00:02:21,609"
+        XCTAssertNil(TimeDuration(sample))
+    }
+    
+    func testSubRip() throws {
+        let sample = """
+        12
+        00:00:55,184 --> 00:00:57,167
+        饿了的话就到我家来
+        Come to the house if you're hungry.
+
+        2
+        00:01:14,805 --> 00:01:16,738
+        谢谢
+        
+         Thank you.
+
+        3
+        00:01:16,738 --> 00:01:19,293
+        嘿 帽子摘了
+         Hey. Take your hat off.
+
+        4
+        00:01:19,293 --> 00:01:21,019
+        噢
+         Oh. Sh
+
+        
+        
+        
+        
+        5
+        00:01:39,347 --> 00:01:41,085
+         谢谢你 亲爱的 不客气
+         Thank you, darling.  You're welcome.
+
+        6
+        00:01:41,125 --> 00:01:42,557
+         祝你今天愉快 你也是
+         Have a good day.  You, too.
+
+        7
+        00:01:44,352 --> 00:01:46,984
+         吉米 你准备好了吗?
+         Jimmy  you ready?
+
+        8
+        00:01:50,393 --> 00:01:52,432
+        噢 准备好了 那行 走吧
+         Uh, yeah.  All right, let's go.
+
+        91
+        00:01:57,641 --> 00:01:59,229
+         谢谢你 女士 不客气
+         Thank you, Ma'am.  You're welcome.
+
+        10
+        00:02:06,064 --> 00:02:07,789
+        你骑的那匹马咋样了？
+         How's the one you rode in on?
+
+        11
+        00:02:07,789 --> 00:02:10,482
+        还没真正驯服 我记得它才两岁
+         Not real broke. I think they said he's just two.
+
+        12
+        00:02:14,175 --> 00:02:16,143
+        我说了 它还没真正驯服呢
+        I said he's not real broke.
+
+        13
+        00:02:16,143 --> 00:02:18,135
+        我们这卖的都是驯服过的 吉米
+         We sell the broke ones around here, Jimmy.
+
+        14
+        
+        00:03:05,975 --> 00:03:07,488
+        妈妈!
+        Mama!
+
+        15
+        00:04:02,282 --> 00:04:04,282
+        翻译：业余渣翻人
+
+        16
+        00:04:17,678 --> 00:04:18,886
+        起这么早干什么?
+         What are you doing up?
+
+        17
+        00:04:18,886 --> 00:04:21,785
+         我爸还没回我电话
+         My father hasn't called me back.
+
+        18
+        00:04:21,785 --> 00:04:23,166
+        我有点担心
+        Worries me.
+        """
+        
+        var index: Int = 0
+        let subrip = SubRip(srtFileContent: sample)
+        subrip.subtitles.forEach { sub in
+            //print(sub.description, terminator: "")
+            
+            index += 1
+            XCTAssertEqual(sub.sequentialNumber, index)
+        }
+        XCTAssertEqual(subrip.subtitles.count, 18)
+        
+        let shiftMilliseconds: Int = 83947
+        let shifted = subrip + shiftMilliseconds
+        let rollback = shifted + (-shiftMilliseconds)
+        
+        
+        XCTAssertEqual(subrip, rollback)
+        
+        let splited = subrip.split()
+        XCTAssertEqual(splited[0].subtitles.count, splited[1].subtitles.count)
+        
     }
 }
